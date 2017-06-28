0e8dd98ad1b67f7d7248a3d42ca974fce4d12063
lufherna <rojoclifford@gmail.com>
2017-06-27 20:47:38 -0400

    added coding to apiRoutes.js

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,23 @@
+// routes
+// this will display the possible friends matches in JSON format
+// ================
+app.get('/date/:matches?', function(req, res) {
+	var datePerson = req.params.matches;
+
+	if (datePerson) {
+		console.log(datePerson);
+
+		for (var i = 0; i < matches.length; i++) {
+			if (datePerson === matches[i].name) {
+				return res.json(matches[i]);
+			}
+		  }
+		return res.json(false);
+	   }
+	return res.json(matches);
+})
+
+// route that takes in incoming survey results
+app.post('/date/new', function(req, res) {
+	
+})
