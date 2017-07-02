0350d01a3fd43d28e834d3dbb0affa2b6975491d
lufherna <rojoclifford@gmail.com>
2017-07-01 21:22:20 -0400

    edited server.js with additional comments and edits

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 // dependancies
 var express = require('express');
 var bodyParser = require('body-parser');
-var path = require('path');
+
+// this does not require path. why though?
+// var path = require('path');
 
 // Sets up Express
-// ===========================
+// this sets up the basic properties for our express server
+// ===============================
+
+// sets an initial port. We'll use this later with our listener
 var PORT = process.env.PORT || 3000;
+
+// tells node that we are creating an express server
 var app = express();
 
 // sets up Express app to handle parsing
+// BodyParser makes it easy for our server to interpret data sent to it
+// standard code below.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text());
@@ -17,11 +26,12 @@ app.use(bodyParser.json({ type: "application/vnd.api+json"}));
 // sets up static assets directories to pull data from (now express can grab stuff from here)
 app.use(express.static('./app/public'));
 
-// node js requires these files in order to route 
+// the below points our server to a series of "route" files
+// these routes give our server a "map" of how to respond when users visit or request various URL
 require('./app/routing/htmlRoutes.js') (app);
 require('./app/routing/apiRoutes.js') (app);
 
-// console logs if it is listening on the proper port
+// the code below 'starts' our server
 app.listen(PORT, function(){
 
 	console.log('Server listening on %d', PORT)
